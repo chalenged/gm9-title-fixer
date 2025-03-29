cf3f2700f314cda824843138a8cd7d9b8de34d9f
chalenged <1337.chalenged@gmail.com>
2025-03-29 12:40:18 -0700

    Many changes; Added verification test for thorough but time-consuming checks, changed to delete from A drive so the permissions page is less scary to end user, added scan mode where nothing is deleted, added simple menu, minor text fixes, probably more

--- title-fixer.lua
+++ title-fixer.lua
@@ -1,5 +1,9 @@
 local log = ""
-local ver = "1.0.0"
+local ver = "1.1.0"
+local encTitle = ""
+local scan = false
+local asked = false
+local allowed = false
 local function L(s) --Simple log function
   log = log.."\n"..s
   print("\n"..s)
@@ -12,6 +16,43 @@ local function sleep (a)
 end
 local function trim_addr (str)
   return string.gsub(str, "/*.*title", "title")
+end
+
+local function delete_title(str, data)
+  str = string.gsub(str, "/*.*title", "A:/title")
+  if allowed and not scan then
+    local su, re = pcall(fs.list_dir, str.."/content")
+    if not su then return L("legit no idea how this happened. Error: "..re) end
+    
+    for lk, lv in pairs(re) do
+      if (string.match(lv.name, ".tmd")) then  --there should only ever be 1 tmd file i think.
+        local su, re = pcall(ui.show_game_info, str
+.."/content/"..lv.name) --Show title information from the A drive, where it is unencrypted, so the user knows what game/app to deal with specifically. 
+        if not su then L("Failed showing game info: "..re) end
+        
+      end
+    end
+    if (not ui.ask("Broken title found: "..trim_addr(str).."\nWould you like it deleted for you?\n (If you don't, you must delete/uninstall/reinstall \nit manually.\nDeny and try to backup your save first if\nyou care!)")) then return L("user denied deletion of "..trim_addr(str)..". Deal with it manually, or run the script again.") end --the file path always gets cut-off here, maybe make a wrapping function that checks length of the string and adds a line break?
+    data_backup = true
+    if (data) then 
+      L("data found, attempting to back it up")
+      local sd, rd = pcall(fs.copy, str.."/data", GM9OUT.."/fixer_backup/"..trim_addr(str).."/data", {recursive=true, overwrite=true})
+      if not sd then 
+      data_backup = false
+        L("Failed to backup data folder: "..rd)
+      else
+        L("Data backed up to "..GM9OUT.."/fixer_backup/data!")
+      end
+    end
+    if not data_backup then
+      if not ui.ask("Data folder found but was not backed up!\nContinue deletion? (save data might be lost!)") then return L("User denied deletion because failed data backup") end
+    end
+    L("Deleting "..str.." folder..")
+    local suc, res = pcall(fs.remove, str, {recursive=true})
+    if not suc then return L("Error deleting folder "..trim_addr(str)..": "..res) end
+  else
+    L("Broken title found: "..trim_addr(str)..". Deal with it manually, or run this script in standard mode.")
+  end
 end
 local function test_title(str) --ran for every folder in title
   --Checks if there is at least one file in the content/cmd folder (this is generally what is used to determine what folder should be deleted, maybe we should check for other things too?)
@@ -50,43 +91,93 @@ local function test_title(str) --ran for every folder in title
     end
   end
   if failed then
-    local su, re = pcall(fs.list_dir, str.."/content")
-    if not su then return L("legit no idea how this happened. Error: "..re) end
-    
-    for lk, lv in pairs(re) do
-      if (string.match(lv.name, ".tmd")) then  --there should only ever be 1 tmd file i think.
-        local su, re = pcall(ui.show_game_info, string.gsub(str, "/*.*title", "A:/title")
-.."/content/"..lv.name) --Show title information from the A drive, where it is unencrypted, so the user knows what game/app to deal with specifically. 
-        if not su then L("Failed showing game info: "..re) end
-        
-      end
-    end
-    if (not ui.ask("Broken title found: "..trim_addr(str).."\n Would you like it deleted for you?\n (If you don't, you must delete/uninstall/reinstall \nit manually.\nDeny and try to backup your save first if\nyou care!)")) then return L("user denied deletion of "..trim_addr(str)..". Deal with it manually, or run the script again.") end --the file path always gets cut-off here, maybe make a wrapping function that checks length of the string and adds a line break?
-    data_backup = true
-    if (data) then 
-      L("data found, attempting to back it up")
-      local sd, rd = pcall(fs.copy, str.."/data", GM9OUT.."/fixer_backup/"..trim_addr(str).."/data", {recursive=true, overwrite=true})
-      if not sd then 
-      data_backup = false
-        L("Failed to backup data folder: "..rd)
-      else
-        L("Data backed up to "..GM9OUT.."/fixer_backup/data!")
-      end
-    end
-    if not data_backup then
-      if not ui.ask("Data folder found but was not backed up!\nContinue deletion? (save data might be lost!)") then return L("User denied deletion because failed data backup)") end
-    end
-    L("Deleting "..str.." folder..")
-    local suc, res = pcall(fs.remove, str, {recursive=true})
-    if not suc then return L("Error deleting folder "..trim_addr(str)..": "..res) end
+    delete_title(str, data)
+    return 1
   end
   
   return 0
 end
+
+local function verify_folder(str) --recursive function to verify all files/folders in path str
+  local success, result = pcall(fs.list_dir, str)
+  if not success then
+    L("Failed listing folder: "..result)
+    return 1
+  end
+  local failCount = 0
+  for ik, iv in pairs(result) do
+    if iv.type == "dir" then
+      failCount = failCount + verify_folder(str.."/"..iv.name)
+    else
+      if not (string.match(iv.name, ".cmd") or string.match(iv.name, ".sav")) then --cmd and sav don't verify. In gm9 you can verify with the cmac, but I don't think it's scriptable
+        local suc, res = pcall(ui.show_game_info, str.."/"..iv.name)
+        if not suc then L("Failed showing game info: "..res) end
+        if not fs.verify(str.."/"..iv.name) then 
+          failCount = failCount+ 1 
+          L(str.."/"..iv.name.." failed verification!")
+        end
+      end
+    end
+  end
+  return failCount
+end
+
+local function verify_title(str)
+  local failed = false
+  local failures = verify_folder(str)
+  if failures > 0 then 
+    failed = true
+    L(tostring(failures).." files failed to verify in "..str)
+    delete_title(str)
+    return 1
+  end
+  return 0
+end
+
+local function ask_perms(force)
+  force = force or false
+  if asked == true and force == false then return
+  elseif asked == true and force == true and allowed == true then 
+    scan = not scan
+    
+    return
+  end
+  asked = true
+  ui.show_text("To fix titles, you must allow permission\nIf you decline, the script will run in \"scan\" mode\nand will only log broken titles, you will\nhave to deal with them yourself")
+  allowed = fs.allow("A:/title/", {opts="ask_all"})
+  scan = not allowed
+end
+
+local function get_option()
+  local opts = {
+    "Quick test (Recommended)",
+    "Verification test",
+    "Toggle scan mode",
+    "Exit"
+  }
+  local scanText = "standard"
+  if scan then scanText = "scan" end
+  ui.show_text("gm9-title-fixer ver "..ver.."\nRunning in "..scanText.." mode\nSelect an option\n\nQuick is recommended. If it doesn't solve\nor find the issue, run the verification test\nNote: The verification test may take a while,\nespecially if you have a lot of titles!")
+  return ui.ask_selection("Choose an option:", opts)
+end
+
+local function show_text(str)
+  local scanText = "standard"
+  if scan then scanText = "scan" end
+  ui.show_text("gm9-title-fixer ver "..ver.."\nRunning in "..scanText.." mode\nTesting "..str)
+end
+
 local function main()
-  --we will need permission to delete things
-  local allowed = fs.allow("0:/Nintendo 3DS/", {opts="ask_all"})
-  if not allowed then return L("Process cancelled by user. Permissions are required for operation.") end
+  --we will need permission to delete things, otherwise run in scan mode
+  ask_perms()
+  local choice = get_option()
+  if choice == nil or choice == 4 then return 0 end
+  if choice == 3 then 
+    ask_perms(true)
+    return 1
+  end
+  
+  if scan then L("Running in scan mode.") else L("Running in standard mode") end
   --simply getting into the id1
   local id0dir = "0:/Nintendo 3DS/"..sys.sys_id0
   local id1 = id0dir
@@ -99,33 +190,53 @@ local function main()
       id1 = id1.."/"..iv["name"]
   end
   L("Running in folder "..id1)
-  local title = id1.."/title" --the other folders in id1 aren't relevant i think
-  local success, tidH = pcall(fs.list_dir, title)
+  encTitle = id1.."/title" --the other folders in id1 aren't relevant i think
+  local success, tidH = pcall(fs.list_dir, encTitle)
   if (not success) then
     return L("Failed getting folders in title:"..tidH)
   end
   --Go through the folders in title (the tid highs)
+  local failures = 0
   for ik, iv in pairs(tidH) do
     if (iv.type ~= "dir") then
       return L(trim_addr(iv.name).." in title is not a folder!")
     end
-    local suc, tidL = pcall(fs.list_dir, title.."/"..iv.name)
+    local suc, tidL = pcall(fs.list_dir, encTitle.."/"..iv.name)
     if (not suc) then
-      return L("Error getting directory list from "..trim_addr(title).."/"..iv.name)
+      return L("Error getting directory list from "..trim_addr(encTitle).."/"..iv.name)
     end
     --test each tid low
     for jk, jv in pairs(tidL) do
-      test_title(title.."/"..iv.name.."/"..jv.name)
+      if choice == 1 then
+        local str = encTitle.."/"..iv.name.."/"..jv.name 
+        show_text(str)
+        failures = failures + test_title(str)
+      elseif choice == 2 then
+        local str = string.gsub(encTitle, "/*.*title", "A:/title").."/"..iv.name.."/"..jv.name 
+        show_text(str)
+        failures = failures + verify_title(str)        
+      end
     end
   end
-  return 0
+  if failures == 0 then
+    L("No broken titles found.")
+    if choice == 1 then ui.echo("Test found no broken titles!\nConsider running a verification test\n")
+    else ui.echo("Test found no broken titles!\nAsk in the Nintendo Homebrew Discord\nfor help, and send them the log.") end
+  else
+    if scan then ui.echo("Test encountered "..tostring(failures).." broken titles.\nDeal with them manually.\nCheck the log for details")
+    else ui.echo("Test encountered "..tostring(failures).." broken titles.\nAny you didn't have the script delete\n must be manually dealt with.\nCheck the log for details") end
+  end
+  return 1 --loops
 end
 
 --instead of gotos i use the return of the main function to determine if the script failed or succeeded
-local result = main()
+local result = 1
+while result == 1 do
+  result = main()
+end 
 if result == 0 then
   L("Process exited without error.")
-  ui.echo("Process ran without error!")
+  --ui.echo("Process ran without error!")
 else
   L("Process exited with error: "..result)
   ui.echo("An error was encountered during operation: \n"..result.."\nCheck the log or send it to someone to check!")
