7f29723576dec9cd88c9f312f9b35d571ba5d1e8
chalenged <1337.chalenged@gmail.com>
2025-06-10 12:57:07 -0700

    Made a few important changes: Added a fallback to attempt deletion from 0: drive if deletion from A: drive fails. Keeps a list of titles that were not fixed, and instructs user to manually delete them. Now ignores title.txt in title folder if it's found (it never strictly caused issues, as it would read it after the folders that matter, but could be confusing) Minor text fixes
    Please let me know if you have any issues or suggestions for this! Feel free to make a Github issue, Pull Request, or to message me on github or Discord! I am in the Nintendo Homebrew Discord as chalenged.

--- title-fixer.lua
+++ title-fixer.lua
@@ -1,9 +1,12 @@
 local log = ""
-local ver = "1.1.2"
+local ver = "1.1.3"
 local encTitle = ""
 local scan = false
 local asked = false
+local blue = false
 local allowed = false
+local fixed = {}
+local broken = {}
 local THEME_TIDLOWS = {["0004008c"]=true, ["00009800"]=true, ["000002cc"]=true, ["00008f00"]=true, ["000002cd"]=true, ["000002ce"]=true} -- this makes checking easier later i promise i know it's cursed okay
 local function L(s) --Simple log function
   log = log.."\n"..s
@@ -18,8 +21,15 @@ end
 local function trim_addr (str)
   return string.gsub(str, "/*.*title", "title")
 end
+local function ask_blue()
+  if (blue) then return true end
+  ui.show_text("A title failed virtual deletion. \nAgree to the permissions on the bottom screen. \nThis is necessary to delete the offending title. \nIf you refuse, you must delete the offending title yourself.\nIf virtual deletion failed")
+  blue = fs.allow("0:/Nintendo 3DS/", {opts="ask_all"})
+  return blue
+end
 
 local function delete_title(str, data)
+  local ogstr = str; --Keep a copy of the original string for deletion from 0: drive and for logging.
   str = string.gsub(str, "/*.*title", "A:/title")
   if allowed and not scan then
     local su, re = pcall(fs.list_dir, str.."/content")
@@ -33,7 +43,10 @@ local function delete_title(str, data)
         
       end
     end
-    if (not ui.ask("Broken title found: "..trim_addr(str).."\nWould you like it deleted for you?\n (If you don't, you must delete/uninstall/reinstall \nit manually.\nDeny and try to backup your save first if\nyou care!)")) then return L("user denied deletion of "..trim_addr(str)..". Deal with it manually, or run the script again.") end --the file path always gets cut-off here, maybe make a wrapping function that checks length of the string and adds a line break?
+    if (not ui.ask("Broken title found: "..trim_addr(str).."\nWould you like it deleted for you?\n (If you don't, you must delete/uninstall/reinstall \nit manually.\nDeny and try to backup your save first if\nyou care!)")) then 
+      table.insert(broken, ogstr)
+      return L("user denied deletion of "..trim_addr(str)..". Deal with it manually, or run the script again.") 
+    end --the file path always gets cut-off here, maybe make a wrapping function that checks length of the string and adds a line break?
     data_backup = true
     if (data) then 
       L("data found, attempting to back it up")
@@ -42,17 +55,37 @@ local function delete_title(str, data)
       data_backup = false
         L("Failed to backup data folder: "..rd)
       else
-        L("Data backed up to "..GM9OUT.."/fixer_backup/data!")
+        L("Data backed up to "..GM9OUT.."/fixer_backup!")
       end
     end
     if not data_backup then
-      if not ui.ask("Data folder found but was not backed up!\nContinue deletion? (save data might be lost!)") then return L("User denied deletion because failed data backup") end
+      if not ui.ask("Data folder found but was not backed up!\nContinue deletion? (save data might be lost!)") then 
+        table.insert(broken, ogstr)
+        return L("User denied deletion because failed data backup") 
+      end
     end
     L("Deleting "..str.." folder..")
     local suc, res = pcall(fs.remove, str, {recursive=true})
-    if not suc then return L("Error deleting folder "..trim_addr(str)..": "..res) end
+    if not suc then --If deleting in A drive fails, attempt deleting in 0 drive and accept the scary blue screen of doom will show. I don't like the idea of modifying the scriptrunner firm to avoid this, and instead will try to get the user to understand it's okay, because it is.
+      L("Error deleting folder "..trim_addr(str)..": "..res..". Attempting deletion from "..ogstr.."instead...") 
+      if ask_blue() then
+        local sul, rel = pcall(fs.remove, ogstr, {recursive=true})
+        if not sul then 
+          table.insert(broken, ogstr)
+          return L("Failed deleting in 0: drive. Is it corrupted? Delete the folder manually!") 
+        else
+          table.insert(fixed, ogstr)          
+        end
+      else
+        L("User denied blue permissions.")
+        table.insert(broken, ogstr)
+      end
+    else
+      table.insert(fixed, ogstr)      
+    end
   else
     L("Broken title found: "..trim_addr(str)..". Deal with it manually, or run this script in standard mode.")
+    table.insert(broken, ogstr)
   end
 end
 local function test_title(str, dlc, theme) --ran for every folder in title
@@ -146,7 +179,7 @@ local function verify_title(str)
   return 0
 end
 
-local function ask_perms(force)
+local function ask_perms(force) --force essentially just means attempt to toggle, without it, nothing happens if perms are given. This allows scan mode even if perms are already given.
   force = force or false
   if asked == true and force == false then return
   elseif asked == true and force == true and allowed == true then 
@@ -169,7 +202,7 @@ local function get_option()
   }
   local scanText = "standard"
   if scan then scanText = "scan" end
-  ui.show_text("gm9-title-fixer ver "..ver.."\nRunning in "..scanText.." mode\nSelect an option\n\nQuick is recommended. If it doesn't solve\nor find the issue, run the verification test\nNote: The verification test may take a while,\nespecially if you have a lot of titles!")
+  ui.show_text("gm9-title-fixer ver "..ver.."\nRunning in "..scanText.." mode\nSelect an option\n\nQuick is recommended. If it doesn't solve\nor find the issue, ask for help\nin the Nintendo Homebrew Discord.\nThe verification test will take a LONG\n time.")
   return ui.ask_selection("Choose an option:", opts)
 end
 
@@ -183,8 +216,8 @@ local function main()
   --we will need permission to delete things, otherwise run in scan mode
   ask_perms()
   local choice = get_option()
-  if choice == nil or choice == 4 then return 0 end
-  if choice == 3 then 
+  if choice == nil or choice == 4 then return 0 end --choice is nil if player pressed b, treat that as exiting
+  if choice == 3 then --ask for perms, toggling if given perms already, asking for perms if not.
     ask_perms(true)
     return 1
   end
@@ -197,7 +230,7 @@ local function main()
   if not successy then
     return L("Failed to initialize id0! Erorr: "..idlist)
   end
-  if (#id1list > 1) then return L("Multiple folders/files in id0! (maybe you have multiple id1, or forgot to remove mset9?)") end --id1 check
+  if (#id1list > 1) then return L("Multiple folders/files in id0! (maybe you have multiple id1, or forgot to remove mset9?)") end --id1 check. Even though the script can technically run despite this, it's possible this is the cause of not having apps show up, as well as often being something that should be avoided.
   for ik,iv in pairs(id1list) do
       id1 = id1.."/"..iv["name"]
   end
@@ -211,47 +244,58 @@ local function main()
   local failures = 0
   for ik, iv in pairs(tidH) do
     if (iv.type ~= "dir") then
-      return L(trim_addr(iv.name).." in title is not a folder!")
-    end
-    local dlc = false
-    if iv.name == "0004008c" then dlc = true end
-    local suc, tidL = pcall(fs.list_dir, encTitle.."/"..iv.name)
-    if (not suc) then
-      return L("Error getting directory list from "..trim_addr(encTitle).."/"..iv.name)
-    end
-    --test each tid low
-    for jk, jv in pairs(tidL) do
-      if choice == 1 then
-        local str = encTitle.."/"..iv.name.."/"..jv.name 
-        show_text(str)
-        local theme = false
-        if THEME_TIDLOWS[jv.name] then theme = true end
-        tested = test_title(str, dlc, theme)
-        if type(verified) == "string" or verified == 1  then
-          failures = failures + 1
+      if (not string.find(iv.name, "^[TtIiLlEe. ()0-9]*.txt")) then 
+        return L(trim_addr(iv.name).." in title is not a folder!")
+      else
+        L(iv.name .. " found in title, ignoring...")
+      end
+    else
+      local dlc = false
+      if iv.name == "0004008c" then dlc = true end --DLC are a bit different than standard titles, so treat them as such.
+      local suc, tidL = pcall(fs.list_dir, encTitle.."/"..iv.name)
+      if (not suc) then
+        return L("Error getting directory list from "..trim_addr(encTitle).."/"..iv.name)
+      end
+      --test each tid low
+      for jk, jv in pairs(tidL) do
+        if choice == 1 then
+          local str = encTitle.."/"..iv.name.."/"..jv.name 
+          show_text(str)
+          local theme = false
+          if THEME_TIDLOWS[jv.name] then theme = true end --check if it's a theme from the array, themes are a bit strange
+          tested = test_title(str, dlc, theme)
+          if type(verified) == "string" or verified == 1  then
+            failures = failures + 1
+          end
+        elseif choice == 2 then
+          local str = string.gsub(encTitle, "/*.*title", "A:/title").."/"..iv.name.."/"..jv.name 
+          show_text(str)
+          verified = verify_title(str)
+          if type(verified) == "string" or verified == 1 then
+            failures = failures + 1
+          end       
         end
-      elseif choice == 2 then
-        local str = string.gsub(encTitle, "/*.*title", "A:/title").."/"..iv.name.."/"..jv.name 
-        show_text(str)
-        verified = verify_title(str)
-        if type(verified) == "string" or verified == 1 then
-          failures = failures + 1
-        end       
       end
     end
   end
-  if failures == 0 then
+  if #broken == 0 and #fixed == 0 then
     L("No broken titles found.")
-    if choice == 1 then ui.echo("Test found no broken titles!\nConsider running a verification test\n")
-    else ui.echo("Test found no broken titles!\nAsk in the Nintendo Homebrew Discord\nfor help, and send them the log.") end
+    ui.echo("Test found no broken titles!\nAsk in the Nintendo Homebrew Discord\nfor help, and send them the log.")
   else
-    if scan then ui.echo("Test encountered "..tostring(failures).." broken titles.\nDeal with them manually.\nCheck the log for details")
-    else ui.echo("Test encountered "..tostring(failures).." broken titles.\nAny you didn't have the script delete\n must be manually dealt with.\nCheck the log for details") end
+    if #broken == 0 then
+      ui.echo("Test fixed "..#fixed.." titles! \nIf your apps still don't show up,\nAsk in the Nintendo Homebrew Discord\nfor help, and send them the log at \n0:/gm9/out/title-fixer_log.txt")
+    elseif #fixed == 0 then
+      ui.echo("Test found "..#broken.." broken titles! Read the log at \n0:/gm9/out/title-fixer_log.txt\nfor more info. A list of broken titles will be at the \nend.Manually delete those folders, backing up \nsaves first if you care about them!")
+    else
+      ui.echo("Test fixed "..#fixed.." titles and \nwas unable to fix "..#broken.." broken titles.\nRead the log at \n0:/gm9/out/title-fixer_log.txt\nfor more info. You may need to delete the \nfolders manually.")
+    end
+    --if scan then ui.echo("Test encountered "..tostring(failures).." broken titles.\nDeal with them manually.\nCheck the log for details")
+    --else ui.echo("Test encountered "..tostring(failures).." broken titles.\nAny you didn't have the script delete\n must be manually dealt with.\nCheck the log for details") end
   end
   return 1 --loops
 end
 
---instead of gotos i use the return of the main function to determine if the script failed or succeeded
+--instead of gotos i use the return of the main function to determine if the script failed or succeeded. Gotos in lua are a bit strange in their restrictions for me.
 local result = 1
 while result == 1 do
   result = main()
@@ -264,6 +308,12 @@ else
   ui.echo("An error was encountered during operation: \n"..result.."\nCheck the log or send it to someone to check!")
 end
 log = "Date and Time: "..os.date().."\nVersion "..ver.."\n---\n"..log
+if #broken > 0 then
+  L("Delete the following folders manually, backing up saves if necessary: ")
+  for ik, iv in pairs(broken) do
+    L(iv)
+  end
+end
 local success, res = pcall(fs.write_file, GM9OUT.."/title-fixer_log.txt", "end", log) --append the log file, then ask to show the user
 if not success then
   L("Failed to open log file! Error: "..res)
